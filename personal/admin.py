12495c1d4d9bb110a717b4b3175fa909c5f12d48
cyberblueprint <rodrigo@rodrigos-MacBook-Pro.local>
2015-12-24 01:33:31 -0300

    Modelos & Admin

--- personal/admin.py
+++ personal/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
 
+
+from .models import Personal, Empresa, Productos, Producto
+
+
 # Register your models here.
+class PersonalAdmin(admin.ModelAdmin):
+	date_hierarchy = 'created_at'
+	search_fields = ['username', 'name']
+	list_display = ['pk', 'username', 'created_at', 'name', 'surname', 'updated_at']
+	#list_filter = ['language', 'category']
+	list_editable = ['username', 'name', 'surname']
+
+
+class EmpresaAdmin(admin.ModelAdmin):
+	date_hierarchy = 'created_at'
+	search_fields = ['username', 'name']
+	list_display = ['pk', 'username', 'name', 'created_at', 'updated_at']
+	#list_filter = ['language', 'category']
+	list_editable = ['username', 'name']
+
+
+class ProductosAdmin(admin.ModelAdmin):
+	date_hierarchy = 'created_at'
+	search_fields = ['username', 'name']
+	list_display = ['pk', 'username', 'empresa', 'show_products', 'updated_at']
+	#list_filter = ['language', 'category']
+	list_editable = ['username']
+
+	def show_products(self, obj):
+		return "\n".join([a.products_set for a in obj.products_set.all()])
+
+
+class ProductoAdmin(admin.ModelAdmin):
+	list_display = ['pk','products_set']
+	#list_filter = ['language', 'category']
+	list_editable = ['products_set']
+
+admin.site.register(Personal, PersonalAdmin)
+admin.site.register(Empresa, EmpresaAdmin)
+admin.site.register(Productos, ProductosAdmin)
+admin.site.register(Producto, ProductoAdmin)
--- personal/models.py
+++ personal/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from datetime import *
 
 # Create your models here.
+class Personal(models.Model):
+	username = models.ForeignKey(User)
+	created_at = models.DateTimeField(auto_now_add=True)
+	name = models.CharField(max_length=30)
+	surname = models.CharField(max_length=30)
+	updated_at = models.DateTimeField(auto_now=True)
+
+
+	def __str__(self):
+		return self.username
+
+class Empresa(models.Model):
+	username = models.ForeignKey(User, default="ipartner")
+	name = models.CharField(max_length=30)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.name
+
+
+PRODUCTOS = (
+	('infraestructura', 'infraestructura'),
+	('software', 'software'),
+	('kpi', 'kpi'),
+	('contratos', 'contratos'), 
+)
+
+
+class Producto(models.Model):
+	products_set = models.CharField(max_length=300, choices=PRODUCTOS)
+
+
+class Productos(models.Model):
+	username = models.ForeignKey(User)
+	empresa = models.ForeignKey(Empresa)
+	products_set = models.ManyToManyField(Producto, related_name="products_all")
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
